6dbcf8b87c0bef91c198ea99159cbf228238e595
lenovo <lenovo@LAPTOP-BLNTEBH7>
2023-06-19 09:05:49 +0530

    this is the redux toolkit configuration

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,7 @@ import counterReducer from '../reducer/counterReducer';
 import rootReducer from '../reducer';
 
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
